1a759befa1a807794a22df4d72857721f6df1e63
yangzhixiong <641484973@qq.com>
2024-12-10 11:31:18 +0800

    add themeswitch button

---
+++ src/components/ThemeSwitch.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="theme-switch">
+    <el-switch 
+      class="el-dark-switch" 
+      v-model="isDark" 
+      @change="toggleTheme" 
+      :active-icon="MoonIcon"
+      :inactive-icon="SunnyIcon" 
+      :inline-prompt="true">
+    </el-switch>
+  </div>
+</template>
+
+<script setup>
+import { useTheme } from '@/theme/useTheme';
+import { Sunny, Moon } from '@element-plus/icons-vue';
+const { isDark, toggleTheme } = useTheme();
+const SunnyIcon = Sunny;
+const MoonIcon = Moon;
+</script>
+
+
+<style>
+.el-dark-switch {
+  --el-switch-off-color: #D8D8DF;
+  --el-switch-on-color: #585858;
+}
+</style>
