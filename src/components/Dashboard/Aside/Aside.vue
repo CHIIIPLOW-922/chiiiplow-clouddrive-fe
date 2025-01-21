48b4403399516e56915a70d458eb64407e72d741
yangzhixiong <641484973@qq.com>
2025-01-21 14:49:54 +0800

    change Aside Layouts 2

--- src/components/Dashboard/Aside/Aside.vue
+++ src/components/Dashboard/Aside/Aside.vue
@@ -2,11 +2,10 @@
   <div class="aside">
     <div class="aside-menu">
       <Menu></Menu>
-      
     </div>
     <div class="aside-progress">
-        <Progress></Progress>
-      </div>
+      <Progress></Progress>
+    </div>
   </div>
 </template>
 
@@ -14,24 +13,29 @@
 import Menu from "@/components/Dashboard/Aside/AsideComponents/Menu.vue";
 import Progress from "@/components/Dashboard/Aside/AsideComponents/Progress.vue";
 </script>
+
 <style lang="scss">
 .aside {
   grid-area: aside;
-  // border: 1px solid black;
-  // overflow-y: auto;
+  display: flex;
+  flex-direction: column; /* 垂直布局 */
   height: 100%;
 
   .aside-menu {
-    // overflow-y: auto;
-    height: 90%;
-    
-  }
-  .aside-progress {
-    width: 100%;
-    padding: 0 20px;
-    position: relative;
-    bottom: 50px;
+    flex: 1; /* 占据剩余空间 */
+    overflow-y: auto; /* 内容超出时滚动 */
   }
   
+  .aside-progress {
+    border-left:1px solid var(--el-border-color);
+    border-right:1px solid var(--el-border-color);
+    width: 100%;
+
+    padding: 0 20px;
+    position: sticky; /* 保持在容器底部 */
+    bottom: 0; /* 距离底部对齐 */
+    padding-bottom: 30px;
+    z-index: 10; /* 保证不被其他内容覆盖 */
+  }
 }
-</style>
+</style> 
